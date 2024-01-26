eebe117daaad513356947726f4c60b4c1e6cc67e
marinagribok <gribok.marina@gmail.com>
2024-01-26 16:35:00 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,100 @@
+const map = new maplibregl.Map({
+    container: 'map', // container id
+    style: 'https://raw.githubusercontent.com/gtitov/basemaps/master/dark-matter-nolabels.json', // style URL
+    center: [0, 0], // starting position [lng, lat]
+    zoom: 1 // starting zoom
+});
+
+fetch("https://docs.google.com/spreadsheets/d/1KO5utL4Ux9AiZtnTeQkzKu25SDCUj3p8AhhYMUbnSxQ/gviz/tq?tqx=out:csv&sheet=Sheet1")
+    .then(response => response.text())
+    .then(text => makeMap(text))
+
+const makeMap = (csvData) => {
+    csv2geojson.csv2geojson(csvData, {
+        latfield: 'lat',
+        lonfield: 'lon',
+        delimiter: ','
+    }, function (error, data) {
+        map.on('load', function () {
+            map.addSource('vacancies', {
+                type: 'geojson',
+                data: data,
+                cluster: true,
+                clusterRadius: 20
+
+            })
+
+            map.addLayer({
+                id: 'clusters',
+                source: 'vacancies',
+                type: 'circle',
+                paint: {
+                    'circle-color': '#7EC8E3',
+                    'circle-stroke-width': 1,
+                    'circle-stroke-color': '#FFFFFF',
+                    'circle-radius': [
+                        'step', 
+                        ['get', 'point_count'],
+                        12,
+                        3,
+                        20,
+                        6,
+                        30
+                    ]
+
+                }
+            })
+
+            map.addLayer({
+                id: 'clusters-labels',
+                type: 'symbol',
+                source: 'vacancies',
+                layout: {
+                    'text-field': '{point_count}',
+                    'text-font': ["Noto Sans Regular"],
+                    'text-size': 12
+                }
+            })
+
+            map.on('click', function(event) {
+                const features = map.queryRenderedFeatures(event.point, {
+                    layers: ['clusters']
+                })
+                if (features[0].properties.cluster_id) {
+
+                const clusterId = features[0].properties.cluster_id
+                const pointCount = features[0].properties.point_count
+                const clusterSource = map.getSource('vacancies')
+
+                clusterSource.getClusterLeaves(
+                    clusterId, 
+                    pointCount, 
+                    0, 
+                    (error, clusterFeatures) => {
+                        //document.getElementById("list").innerHTML = clusterFeatures
+                        clusterFeatures.map(feature => {
+                            document.getElementById('list').innerHTML += `<h4>${feature.properties["Вакансия"]}</h4><a href="${feature.properties["Ссылка на сайте Картетики"]}'>Подробнее</a><hr>"`
+                        })
+                    }
+                );
+            } else {
+                const unclusteredFeature = features[0]
+                document.getElementById('list').innerHTML = unclusteredFeature
+                console.log(unclusteredFeature)
+            }
+                
+                
+                console.log(features)
+
+            })
+
+            map.on("mouseenter", "clusters", function () {
+                map.getCanvas().style.cursor = "pointer"
+            })
+
+            map.on("mouseleave", "clusters", function () {
+                map.getCanvas().style.cursor = ""
+            })
+        })
+    })
+}
